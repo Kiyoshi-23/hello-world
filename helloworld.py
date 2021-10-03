6f6705c5b8625703cecff0e26911affa75f82b99
Sashi <90008981+Sashi-boi@users.noreply.github.com>
2021-10-03 09:56:54 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,6 @@
 print("hello world")
-#very simple code
+#very simple code using only the print function. 
+
+#the word 'print' tells the computer what type of fuction the code is.
+
+#The phrase or word inside the parenthesis tells the computer what to print.
