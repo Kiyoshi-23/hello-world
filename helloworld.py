1665d94c29ee22dbbaaff09ceea8f033f3eeec35
Sashi <90008981+Sashi-boi@users.noreply.github.com>
2021-10-03 15:41:26 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,4 @@
 print("hello world, this is my first github repository.")
-#very simple code using only the print function. 
-
-#the word 'print' tells the computer what type of fuction the code is.
-
-#The phrase or word inside the parenthesis tells the computer what to print.
 
 info = "I'm just a teenager who loves technology and has 6 fingers."
 follow = "Follow me on my github to see more code."
