74edb1d0825a8c5e8737bbf2786a18aacde44fd0
Sashwath <90008981+Kiyoshi-23@users.noreply.github.com>
2022-02-11 18:19:56 -0600

    and push

--- helloworld.py
+++ helloworld.py
@@ -7,3 +7,9 @@ reach_out = "You can react me at my discord: ☆✧ Kiyoshi#5246"
 print(info)
 print(follow)
 print(reach_out)
+
+# this program introduces "print()." The functions wants you to put some text in the inside of the () using ''.
+# don't get it just yet? just copy the text above, replace the "Hello World" text with your own, and then run it!
+# this code also introduces comments, which are started by an "#." They don't affect your code, and will be used to explain a bit more about any given line of code.
+# by using a variable to generate the code and using it, it will print out whatever is defined the variable. more complex, but still works!
+
