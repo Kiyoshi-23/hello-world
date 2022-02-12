d29407ee7ac418902cd745c4670ab3c50c13cada
Sashwath <90008981+Kiyoshi-23@users.noreply.github.com>
2022-02-11 18:07:27 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ print("hello world, this is my first github repository.")
 
 info = "I'm just a teenager who loves technology and has 6 fingers."
 follow = "Follow me on my github to see more code."
-reach_out = "You can react me at my discord: 良いボイ#1689"
+reach_out = "You can react me at my discord: ☆✧ Kiyoshi#5246"
 
 print(info)
 print(follow)
